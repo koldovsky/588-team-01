d8fc8eb8d7a75c9747354700b75e995820fc053f
Ronja99 <rostik.shargorodsky@gmail.com>
2021-04-27 18:43:04 +0300

    added responsive for header section

--- js/partners.js
+++ js/partners.js
@@ -1,6 +1,27 @@
-var swiper = new Swiper(".swiper-container", {
-  slidesPerView: 6,
+let swiper = new Swiper(".swiper-container", {
+  slidesPerView: 1,
   spaceBetween: 0,
+  breakpoints: {
+    // when window width is >= 425px
+    100: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    // when window width is >= 425px
+    427: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+    // when window width is >= 980px
+    980: {
+      slidesPerView: 4,
+      spaceBetween: 40,
+    },
+    1024: {
+      slidesPerView: 6,
+      spaceBetween: 0,
+    },
+  },
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
