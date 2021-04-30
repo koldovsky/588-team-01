d10fe0a0e576aa4f2ae937e190821bc4ccd66a72
Oleg0180 <oksanabakin656@gmail.com>
2021-04-30 10:56:36 +0000

    Buno Oleg fixed my part

--- js/timer-b-sell.js
+++ js/timer-b-sell.js
@@ -1,25 +1,25 @@
-let date = new Date('Jun 1 2021 00:00:00') 
+(function(){
+    let date = new Date('Jun 1 2021 00:00:00') 
 
-function countdown(){
-    let now = new Date();
-    gap = date - now;
-    
-    let days = Math.floor(gap / 1000 / 60 / 60 / 24);
-    let hours = Math.floor(gap / 1000 / 60 / 60) % 24;
-    let minutes = Math.floor(gap / 1000 / 60 ) % 60;
-    let seconds = Math.floor(gap / 1000 ) % 60;
+    function countdown(){
+        let now = new Date();
+        const gap = date - now;
+        
+        let days = Math.floor(gap / 1000 / 60 / 60 / 24);
+        let hours = Math.floor(gap / 1000 / 60 / 60) % 24;
+        let minutes = Math.floor(gap / 1000 / 60 ) % 60;
+        let seconds = Math.floor(gap / 1000 ) % 60;
 
-    if(gap < 0){document.getElementById('timer').innerText = "Розходимось"
-    }else{
-    document.getElementById('d').innerText = days;
-    document.getElementById('h').innerText = hours;
-    document.getElementById('m').innerText = minutes;
-    document.getElementById('s').innerText = seconds;
-    console.log(gap);
+        if(gap < 0){document.getElementById('timer').innerText = "Розходимось"
+        }else{
+        document.getElementById('d').innerText = days;
+        document.getElementById('h').innerText = hours;
+        document.getElementById('m').innerText = minutes;
+        document.getElementById('s').innerText = seconds;
+        }
     }
-}
 
-countdown();
+    countdown();
 
-setInterval(countdown, 1000);
-
+    setInterval(countdown, 1000);
+})();
